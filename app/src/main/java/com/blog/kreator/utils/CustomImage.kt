76c0996338a607e6caeedac340ebdf80249db3f7
SumitSoni-001 <geekysumit24680@gmail.com>
2022-12-26 16:31:32 +0530

    Minor changes

--- app/src/main/java/com/blog/kreator/utils/CustomImage.kt
+++ app/src/main/java/com/blog/kreator/utils/CustomImage.kt
@@ -1,20 +1,22 @@
 package com.blog.kreator.utils
 
-object CustomImage {
+class CustomImage {
 
-    private fun userNameImage(name: String): String {
-        return "https://ui-avatars.com/api?name=$name&background=random"
-    }
+    companion object {
+        private fun userNameImage(name: String): String {
+            return "https://ui-avatars.com/api?name=$name&background=random"
+        }
 
-    fun downloadImage(image: String): String {
-        return "${Constants.BASE_URL}/api/post/downloadImage/$image"
-    }
+        fun downloadImage(image: String): String {
+            return "${Constants.BASE_URL}/api/post/downloadImage/$image"
+        }
 
-    fun downloadProfile(image: String?, name: String): String {
-        return if (image == null || image == "default.png") {
-            userNameImage(name)
-        } else {
-            "${Constants.BASE_URL}/api/users/downloadProfilePic/$image"
+        fun downloadProfile(image: String?, name: String): String {
+            return if (image == null || image == "default.png") {
+                userNameImage(name)
+            } else {
+                "${Constants.BASE_URL}/api/users/downloadProfilePic/$image"
+            }
         }
     }
 
--- app/src/main/java/com/blog/kreator/utils/FormatTime.kt
+++ app/src/main/java/com/blog/kreator/utils/FormatTime.kt
@@ -7,57 +7,68 @@ import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
-object FormatTime {
-    fun getFormattedTime(dateTime:String) : String{
-        val date = dateTime.substring(0,10)
-        val time = dateTime.substring(11,19)
-        val year = dateTime.substring(0,4).toInt()
-        val month = dateTime.substring(5,7).toInt()
-        val day = dateTime.substring(8,10).toInt()
-        val hour = dateTime.substring(11,13)
-        val minute = dateTime.substring(14,16)
-        val seconds = dateTime.substring(17,19)
+class FormatTime {
 
-        val outputFormat: DateFormat = SimpleDateFormat("EEE, dd MMM", Locale.ENGLISH)
-        val inputFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
-        val inputText: String = date
-        var getDate: Date? = null
-        try {
-            getDate = inputFormat.parse(inputText)
-        } catch (e: ParseException) {
-            e.printStackTrace()
-            Log.e("Title", "------" + e.message.toString() + " = " + inputText)
-        }
-        var output = ""
+    companion object {
+        fun getFormattedTime(dateTime: String): String {
+            val date = dateTime.substring(0, 10)
+            val time = dateTime.substring(11, 19)
+            val year = dateTime.substring(0, 4).toInt()
+            val month = dateTime.substring(5, 7).toInt()
+            val day = dateTime.substring(8, 10).toInt()
+            val hour = dateTime.substring(11, 13)
+            val minute = dateTime.substring(14, 16)
+            val seconds = dateTime.substring(17, 19)
 
-        /** DateTime Functionality */
-        if (date.equals(org.threeten.bp.LocalDate.now().toString(),true)){
-            output = if (time.equals(org.threeten.bp.LocalTime.now().toString(),true)){
-                "Just now"
-            }else if (hour.equals(org.threeten.bp.LocalTime.now().hour.toString(),true)){
-                if (minute.equals(org.threeten.bp.LocalTime.now().minute.toString(),true)){
-                    "${org.threeten.bp.LocalTime.now().second.minus(seconds.toInt())} seconds ago"
-                }else{
-                    "${org.threeten.bp.LocalTime.now().minute.minus(minute.toInt())} minutes ago"
+            val outputFormat: DateFormat = SimpleDateFormat("EEE, dd MMM", Locale.ENGLISH)
+            val inputFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
+            val inputText: String = date
+            var getDate: Date? = null
+            try {
+                getDate = inputFormat.parse(inputText)
+            } catch (e: ParseException) {
+                e.printStackTrace()
+                Log.e("Title", "------" + e.message.toString() + " = " + inputText)
+            }
+            var output = ""
+
+            /** DateTime Functionality */
+            if (date.equals(org.threeten.bp.LocalDate.now().toString(), true)) {
+                output = if (time.equals(org.threeten.bp.LocalTime.now().toString(), true)) {
+                    "Just now"
+                } else if (hour.equals(org.threeten.bp.LocalTime.now().hour.toString(), true)) {
+                    if (minute.equals(org.threeten.bp.LocalTime.now().minute.toString(), true)) {
+                        "${org.threeten.bp.LocalTime.now().second.minus(seconds.toInt())} seconds ago"
+                    } else {
+                        "${org.threeten.bp.LocalTime.now().minute.minus(minute.toInt())} minutes ago"
+                    }
+                } else {
+                    "${org.threeten.bp.LocalTime.now().hour.minus(hour.toInt())} hours ago"
                 }
-            }else{
-                "${org.threeten.bp.LocalTime.now().hour.minus(hour.toInt())} hours ago"
-            }
-        }else if(date.equals(org.threeten.bp.LocalDate.now().minusDays(1).toString(),true)){
-            output = "Yesterday"
-        }else if(org.threeten.bp.LocalDate.of(year, month, day).get(ChronoField.MONTH_OF_YEAR) == org.threeten.bp.LocalDate.now().get(
-                ChronoField.MONTH_OF_YEAR)) {
-            output = if (org.threeten.bp.LocalDate.of(year, month, day).get(ChronoField.ALIGNED_WEEK_OF_MONTH) == org.threeten.bp.LocalDate.now().get(
-                    ChronoField.ALIGNED_WEEK_OF_MONTH)) {
+            } else if (date.equals(org.threeten.bp.LocalDate.now().minusDays(1).toString(), true)) {
+                output = "Yesterday"
+            } else if (org.threeten.bp.LocalDate.of(year, month, day)
+                    .get(ChronoField.MONTH_OF_YEAR) == org.threeten.bp.LocalDate.now().get(
+                    ChronoField.MONTH_OF_YEAR
+                )
+            ) {
+                output = if (org.threeten.bp.LocalDate.of(year, month, day)
+                        .get(ChronoField.ALIGNED_WEEK_OF_MONTH) == org.threeten.bp.LocalDate.now()
+                        .get(
+                            ChronoField.ALIGNED_WEEK_OF_MONTH
+                        )
+                ) {
 //                "This Week"
-                (org.threeten.bp.LocalDate.now().dayOfMonth - day).toString() + " days ago"
+                    (org.threeten.bp.LocalDate.now().dayOfMonth - day).toString() + " days ago"
+                } else {
+                    outputFormat.format(getDate!!)
+                }
             } else {
-                outputFormat.format(getDate!!)
+                output = outputFormat.format(getDate!!)
             }
-        } else {
-            output = outputFormat.format(getDate!!)
-        }
 
-        return output
+            return output
+        }
     }
+
 }
